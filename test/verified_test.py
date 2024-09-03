b07e05240e1284bee718b03679fa403f6eda007b
Sepand Haghighi <sepand.haghighi@yahoo.com>
2024-09-04 00:23:15 +0330

    5 new methods (#8)
    * feat : 5 new methods added
    
    * fix : tests updated
    
    * fix : tests updated
    
    * fix : minor edit in functions test
    
    * doc : METHODS.md updated
    
    * doc : CHANGELOG.md updated
    
    * fix : minor edit in cold-brew-conc info
    
    * doc : references updated

--- test/verified_test.py
+++ test/verified_test.py
@@ -51,4 +51,54 @@ True
 >>> french_press_coffee = coffee_calc(french_press_params)
 >>> french_press_coffee == 8
 True
+>>> pour_over_params = load_method_params("pour-over") # https://www.nicolebattefeld.com/post/best-recipes-2022
+>>> pour_over_params["coffee_ratio"] == 1
+True
+>>> pour_over_params["water_ratio"] == 15
+True
+>>> pour_over_params["water"] == 240
+True
+>>> pour_over_coffee = coffee_calc(pour_over_params)
+>>> pour_over_coffee == 16
+True
+>>> auto_drip_params = load_method_params("auto-drip") # https://wonderstate.com/pages/auto-drip
+>>> auto_drip_params["coffee_ratio"] == 1
+True
+>>> auto_drip_params["water_ratio"] == 16
+True
+>>> auto_drip_params["water"] == 128
+True
+>>> auto_drip_coffee = coffee_calc(auto_drip_params)
+>>> auto_drip_coffee == 8
+True
+>>> cold_brew_params = load_method_params("cold-brew") # https://counterculturecoffee.com/blogs/counter-culture-coffee/guide-to-cold-brew
+>>> cold_brew_params["coffee_ratio"] == 1
+True
+>>> cold_brew_params["water_ratio"] == 11
+True
+>>> cold_brew_params["water"] == 242
+True
+>>> cold_brew_coffee = coffee_calc(cold_brew_params)
+>>> cold_brew_coffee == 22
+True
+>>> cold_brew_conc_params = load_method_params("cold-brew-conc") # https://www.thespruceeats.com/cold-brew-concentrate-recipe-5197494
+>>> cold_brew_conc_params["coffee_ratio"] == 1
+True
+>>> cold_brew_conc_params["water_ratio"] == 5
+True
+>>> cold_brew_conc_params["water"] == 120
+True
+>>> cold_brew_conc_coffee = coffee_calc(cold_brew_conc_params)
+>>> cold_brew_conc_coffee == 24
+True
+>>> moka_pot_params = load_method_params("moka-pot") # https://bakedbrewedbeautiful.com/how-to-make-coffee-in-moka-pot
+>>> moka_pot_params["coffee_ratio"] == 1
+True
+>>> moka_pot_params["water_ratio"] == 10
+True
+>>> moka_pot_params["water"] == 60
+True
+>>> moka_pot_coffee = coffee_calc(moka_pot_params)
+>>> moka_pot_coffee == 6
+True
 """
