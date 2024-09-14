0f880c1adf79b2ab501ee9079516fb1373e173a0
sepandhaghighi <sepand.haghighi@yahoo.com>
2024-09-14 19:50:38 +0430

    rel : minor edit in functions_test.py

--- test/functions_test.py
+++ test/functions_test.py
@@ -107,7 +107,7 @@ True
 >>> _ = parser.add_argument('--version', help='version', nargs="?", const=1)
 >>> args = parser.parse_args({"--version":True})
 >>> run(args)
-0.1
+0.2
 >>>
 >>> args = parser.parse_args(["--method", 'v60'])
 >>> run(args)
