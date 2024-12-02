1377b5669c92591db0196d2e30450891e4dcce2f
sepandhaghighi <sepand.haghighi@yahoo.com>
2024-12-02 18:52:16 +0330

    feat : cup added to WATER_UNITS_MAP

--- mycoffee/params.py
+++ mycoffee/params.py
@@ -215,4 +215,5 @@ WATER_UNITS_MAP = {
     "tbsp": {"name": "tablespoon", "rate": 0.067628},
     "tsp": {"name": "teaspoon", "rate": 0.20288},
     "dsp": {"name": "dessertspoon", "rate": 0.10144},
+    "cup": {"name": "cup", "rate": 0.0042268},
 }
