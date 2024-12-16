3e09302fda05dbd3db8a3f858d528b3eb2cab732
sepandhaghighi <sepand.haghighi@yahoo.com>
2024-12-16 09:46:35 +0330

    rel : migrate to version 1.0

--- test/functions_test.py
+++ test/functions_test.py
@@ -220,7 +220,7 @@ True
 >>> _ = parser.add_argument('--version', help='version', nargs="?", const=1)
 >>> args = parser.parse_args({"--version":True})
 >>> run(args)
-0.9
+1.0
 >>>
 >>> args = parser.parse_args(["--method", 'v60'])
 >>> run(args)
