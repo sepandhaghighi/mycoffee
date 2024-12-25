775c371d217dddad7fe6b73c4b69ae3b7f79eae8
Sadra Sabouri <sabouri.sadra@gmail.com>
2024-12-25 09:49:41 -0800

    add : `grains` unit added for water and coffee.

--- mycoffee/params.py
+++ mycoffee/params.py
@@ -203,6 +203,7 @@ COFFEE_UNITS_MAP = {
     "dsp": {"name": "dessertspoon", "rate": 0.27792},
     "cup": {"name": "cup", "rate": 0.01158},
     "t oz": {"name": "troy ounce", "rate": 0.032151},
+    "gr": {"name": "grain", "rate": 15.4324},
 }
 
 WATER_UNITS_MAP = {
@@ -221,4 +222,5 @@ WATER_UNITS_MAP = {
     "qt": {"name": "quart", "rate": 0.00105669},
     "fl oz": {"name": "fluid ounce", "rate": 0.033814},
     "t oz": {"name": "troy ounce", "rate": 0.032151},
+    "gr": {"name": "grain", "rate": 15.4324},
 }
