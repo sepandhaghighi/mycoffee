bb515ecd1314492edf215ffae9550b0a8dd7f747
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-04-19 19:08:02 +0330

    fix : get_result function updated

--- mycoffee/functions.py
+++ mycoffee/functions.py
@@ -345,6 +345,7 @@ def filter_params(params: Dict[str, Union[str, int, float]]) -> Dict[str, Union[
     params["coffee"]["total"] = round(params["coffee"]["total"], digits)
     params["water"]["cup"] = round(params["water"]["cup"], digits)
     params["water"]["total"] = round(params["water"]["total"], digits)
+    params["ratio"] = round(params["ratio"], digits)
     if is_int(params["coffee"]["cup"]):
         params["coffee"]["cup"] = int(params["coffee"]["cup"])
     if is_int(params["coffee"]["total"]):
@@ -494,14 +495,14 @@ def get_result(params: Dict[str, Union[str, int, float]]) -> Dict[str, Union[str
     :param params: parameters
     """
     params_copy = params.copy()
+    params_copy["ratio"] = params_copy["coffee_ratio"] / params_copy["water_ratio"]
     params_copy["coffee"] = dict()
     params_copy["water"] = dict()
-    params_copy["coffee"]["cup"] = calc_coffee(params)
+    params_copy["coffee"]["cup"] = calc_coffee(ratio=params_copy["ratio"], water=params["water"], water_unit=params["water_unit"], coffee_unit=params["coffee_unit"])
     params_copy["coffee"]["total"] = params_copy["cups"] * params_copy["coffee"]["cup"]
     params_copy["water"]["cup"] = params["water"]
     params_copy["water"]["total"] = params_copy["cups"] * params_copy["water"]["cup"]
     params_copy["grind_type"] = get_grind_type(params_copy["grind"])
-    params_copy["ratio"] = round(params_copy["coffee_ratio"] / params_copy["water_ratio"], params["digits"])
     params_copy["strength"] = get_brew_strength(ratio=params_copy["ratio"])
     params_copy["grind_unit"] = "um"
     result = filter_params(params_copy)
